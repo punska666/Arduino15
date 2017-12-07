691f419dbfe2ac787cdcb3e9b06ee1f188a76132
Pierre valleau <pierre.valleau@microchip.com>
2017-12-07 18:07:33 +0100

    try to deploy

--- packages/zoubworld_Arduino/build.bash
+++ packages/zoubworld_Arduino/build.bash
@@ -1,10 +1,7 @@
-cd tools
-./build.bash
-cd ..
+cd tools ; ./build.bash ; cd ..
 
-cd hardware/samd/*/
-./build.bash
-cd ../../..
+
+cd hardware/samd/*/ ; ./build.bash ; cd ../../..
 
 mkdir Release
 cp -Rf hardware/samd/1*/Release/ ./
@@ -13,7 +10,7 @@ cp -Rf tools/Release/ ./
 echo  "" >./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '{'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '  "packages": ['>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
-
+echo '  {   '>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo ' "name": "zoubworld_Arduino",'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '      "maintainer": "zoubworld LLC",'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '      "websiteURL": "http://lfbn-1-14132-125.w90-12.abo.wanadoo.fr/~zoubworld_Arduino/",'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
@@ -22,7 +19,7 @@ echo '      "help": {'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '        "online": "http://lfbn-1-14132-125.w90-12.abo.wanadoo.fr/~zoubworld_Arduino/support/"'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '      },'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '      "platforms": ['>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
-echo '        {'>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
+echo '        '>>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 		
 cat  Release/package_Zoubworld_sam_m0p-1.6.16-beta-b_index.json >>./Release/package_Zoubworld_1.6.16-beta-b_index.json
 echo '   ],' >>./Release/package_Zoubworld_1.6.16-beta-b_index.json
